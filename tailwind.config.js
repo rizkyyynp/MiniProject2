b3cf715a697fe117deb3ca179804e79a2f81e0ac
Rizky Nugraha <rizkynp990@gmail.com>
2024-05-29 22:03:13 +0700

    Add feature Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,42 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#38d39f',
+        'secondary': '#FFF0EB',
+        'third': '#274546',
+        'inputs': '#555',
+        'inputs2': '#555',
+        'links': '#999',
+        'h2T': '#333',
+      },
+      width:{
+        '12.5':'50px',
+        '15':'60px',
+        '25': '100px',
+        '70': '280px',
+        '75': '300px',
+      },
+      height:{
+        '12.5':'50px',
+      },
+      boxShadow:{
+        'bS1': '0 20px 35px rgba(0, 0, 0, 0.102)',
+      },
+      borderRadius:{
+        '1/2' : '50%',
+      },
+      fontWeight: {
+        '15px' : '15px',
+      },
+      fontSize: {
+        '2px' : '2px',
+      },
+      padding: {
+        '3.75' : '15px',
+      },
+    },
   },
   plugins: [],
 }
